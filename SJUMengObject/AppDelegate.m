ca362323ec4edb3736a1f562004d7e44167d7877
宋江 <2496157421@qq.com>
2021-07-12 14:19:59 +0800

    替换qq AppID

--- SJUMengObject/AppDelegate.m
+++ SJUMengObject/AppDelegate.m
@@ -94,7 +94,7 @@
     /* 设置分享到QQ互联的appID
          * U-Share SDK为了兼容大部分平台命名，统一用appKey和appSecret进行参数设置，而QQ平台仅需将appID作为U-Share的appKey参数传进即可。
         */
-    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_QQ appKey:@"1106271196" appSecret:nil redirectURL:nil];
+    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_QQ appKey:@"101955131" appSecret:nil redirectURL:nil];
     /* 设置新浪的appKey和appSecret */
     [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_Sina appKey:@"3921700954"  appSecret:nil redirectURL:nil];
     
